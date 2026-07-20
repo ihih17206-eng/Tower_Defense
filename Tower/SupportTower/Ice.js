879ed876f5f7c4966f9a01c104bd1667d8e9f4ee
ihih17206-eng <ihih17206@gmail.com>
2026-07-20 22:53:20 +0800

    Add files via upload

---
+++ Tower/SupportTower/Ice.js
@@ -0,0 +1,5 @@
+class Ice extends Tower {
+  constructor(x, y) {
+    super(x, y, 'ice');
+  }
+}
---
+++ Tower/SupportTower/Protection_Totem.js
@@ -0,0 +1,5 @@
+class ProtectionTotem extends Tower {
+  constructor(x, y) {
+    super(x, y, 'protectionTotem');
+  }
+}
